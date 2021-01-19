682801e40116a323c0bbfce0233f415ca0ca3584
Kamil Serafin <kamilserafindev@gmail.com>
2021-01-19 21:15:18 +0100

    Add fonts

--- components/social.jsx
+++ components/social.jsx
@@ -1,31 +1,38 @@
 import classNames from "classnames";
-import {useMenu} from "../hooks/useMenu";
+import { useMenu } from "../hooks/useMenu";
+import Image from "next/image";
 
 export default function Social(props) {
-  const menu = useMenu()
+  const menu = useMenu();
 
   let socialClasses = classNames({
-    'mt-5 md:mt-3 lg:mt-6 md:leading-200': !props.flex
-  })
+    "mt-5 md:mt-3 lg:mt-6 md:leading-200": !props.flex,
+  });
 
   let flex = classNames({
-    'flex justify-between w-22': props.flex,
-  })
+    "flex justify-between w-22": props.flex,
+  });
 
-  let icons = []
+  let icons = [];
   for (const el of menu.social_media) {
     icons.push(
-      <a className="cursor-pointer" key={el.social_media_object.social_media_object_icon.alt} target="_blank" href={el.social_media_object.social_media_object_link}>
+      <a
+        className="cursor-pointer"
+        key={el.social_media_object.social_media_object_icon.alt}
+        target="_blank"
+        href={el.social_media_object.social_media_object_link}
+      >
         <div className={socialClasses}>
-          <img src={el.social_media_object.social_media_object_icon.url} alt={el.social_media_object.social_media_object_icon.alt}/>
+          <Image
+            src={el.social_media_object.social_media_object_icon.url}
+            alt={el.social_media_object.social_media_object_icon.alt}
+            width={22}
+            height={22}
+          />
         </div>
       </a>
-    )
+    );
   }
 
-  return (
-      <div className={flex}>
-        { icons }
-      </div>
-    )
+  return <div className={flex}>{icons}</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       "./components/**/*.jsx",
       "**/*.html",
       "**/*.svg",
+      "**/*.woff2",
+      "**/*.woff",
     ],
     options: {
       safelist: [
