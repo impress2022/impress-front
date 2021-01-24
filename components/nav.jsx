eb38f63a30e784dd82a1497d5e7ebe6ae8f12b43
Kamil Serafin <wa12rior@gmail.com>
2021-01-24 22:47:10 +0100

    Work on menu

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Hamburger from "./hamburger";
-import {useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import classNames from "classnames";
 import Social from "./social";
 import Details from "./contact/details";
@@ -8,6 +8,8 @@ import NavMenu from "./navMenu";
 
 function Nav() {
   const [menuToggle, setMenuToggle] = useState(false);
+  const [menuHide, setMenuHide] = useState(false);
+  const scrollOffsetY = useRef(0);
 
   let menuClasses = classNames({
     'transition-all duration-500 ease-in-out z-20 nav-wrapper bg-white fixed h-screen top-0 w-screen pt-24 md:pt-28 lg:pt-52 flex': true,
@@ -21,8 +23,35 @@ function Nav() {
     '-left-7': menuToggle
   })
 
+  useEffect(() => {
+    // only execute all the code below in client side
+    if (typeof window !== "undefined") {
+
+      function handleScroll() {
+        let currentScrollPos = window.pageYOffset;
+        console.log(menuHide)
+        if (scrollOffsetY.current > currentScrollPos) {
+          // show
+          setMenuHide(false);
+        } else {
+          // hide
+          setMenuHide(true);
+        }
+        scrollOffsetY.current = window.pageYOffset;
+      }
+
+      // Add event listener
+      window.addEventListener("scroll", handleScroll, { passive: true });
+
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, [menuHide]); // Empty array ensures that effect is only run on mount
+
   return (
-    <div className="h-20 lg:h-nav mx-7 md:mx-15">
+    <div className="sticky bg-white top-0 transition-transform ease-in h-20 lg:h-nav mx-7 md:mx-15" style={{
+      transform: menuHide ? "translateY(-5rem)" : "translateY(0)"
+    }}>
       <nav className="flex justify-between items-center h-full">
         <div>
           <Link href="/"><a><img src="/images/Logo.svg" alt="Impress"/></a></Link>
